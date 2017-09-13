2c4e17d60055ac933e7bfd689877f7d4c77c0e28
Yasuhiko ITOH <itouya@alpha.co.jp>
2017-09-13 17:25:17 +0900

    tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { Component }          from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-//  template: `
-//    <h1>{{title}}</h1>
-//    <nav>
-//      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-//      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-//    </nav>
-//    <router-outlet></router-outlet>
-//  `,
+  //template: `
+  //  <h1>{{title}}</h1>
+  //  <nav>
+  //    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+  //    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+  //  </nav>
+  //  <router-outlet></router-outlet>
+  //`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
