987644ad80197557aeced993447875f8024a12c8
伊藤靖彦 <yasuhiko@chelonia.local>
2017-10-22 23:04:43 +0900

    イメージアップロード

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
 import { NgbActiveModal }   from '@ng-bootstrap/ng-bootstrap';
 
 import { Hero }        from './hero';
@@ -11,14 +11,34 @@ import { HeroService } from './hero.service';
 })
 export class HeroDetailComponent {
   @Input() hero: Hero;
+  private _el: HTMLElement;
+  fileChange = false;
 
   constructor(
     private heroService: HeroService,
-    private activeModal: NgbActiveModal
-  ) {}
+    private activeModal: NgbActiveModal,
+    private el: ElementRef
+  ) {
+    this._el = this.el.nativeElement;
+  }
 
   save(): void {
     this.heroService.update(this.hero)
       .subscribe(() => this.activeModal.close('Close Click'));
+  }
+
+  previewImage(fileInput: any): void {
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = ((e) => {
+        this._el.querySelector('.card-img-top').setAttribute('src', e.target['result']);
+        this.fileChange = true;
+      });
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+  }
+
+  fileUpload(): void {
+    console.log('image upload ...');
   }
 }
