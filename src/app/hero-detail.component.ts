5651efb7c59535912e0c69bcab8a64ba74bf1e37
Yasuhiko ITOH <itouya@alpha.co.jp>
2017-10-23 20:59:20 +0900

    server-url/upload にアップロードできるようにした

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
 import { NgbActiveModal }   from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient } from '@angular/common/http';
 
 import { Hero }        from './hero';
 import { HeroService } from './hero.service';
@@ -11,14 +12,46 @@ import { HeroService } from './hero.service';
 })
 export class HeroDetailComponent {
   @Input() hero: Hero;
+  private _el: HTMLElement;
+  @ViewChild('accountCard') cardEl: ElementRef;
+  isUnchanged = true;
+  uploadfile: any;
 
   constructor(
     private heroService: HeroService,
-    private activeModal: NgbActiveModal
+    private activeModal: NgbActiveModal,
+    private http: HttpClient
   ) {}
 
   save(): void {
     this.heroService.update(this.hero)
       .subscribe(() => this.activeModal.close('Close Click'));
+  }
+
+  preview(fileInput: any): void {
+    this._el = this.cardEl.nativeElement;
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = ((e) => {
+        this._el.querySelector('.card-img-top').setAttribute('src', e.target['result']);
+        this.isUnchanged = false;
+      });
+      reader.readAsDataURL(fileInput.target.files[0]);
+      this.uploadfile = fileInput.target.files[0];
+    }
+  }
+
+  upload(): void {
+    console.log('upload...');
+    let formData = new FormData();
+    formData.append('account_image', this.uploadfile);
+    formData.append('name', this.hero.name);
+    formData.append('id', this.hero.id.toString());
+    this.http
+      .post('http://localhost:8080/upload', formData)
+      .subscribe(response => {
+        console.log(response);
+      });
+    this.isUnchanged = true;
   }
 }
